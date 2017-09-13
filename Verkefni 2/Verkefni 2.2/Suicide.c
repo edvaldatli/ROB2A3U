08f0212d7806154fef384c699cba33c97ff92e78
Aron Hrafnsson <aronhr98@gmail.com>
2017-09-13 11:32:35 +0000

    Suicide with Encoders

--- Verkefni 2/Verkefni 2.2/Suicide.c
+++ Verkefni 2/Verkefni 2.2/Suicide.c
@@ -1,36 +1,71 @@
-#pragma config(Sensor, dgtl1,  rightEncoder,   sensorQuadEncoder)
-#pragma config(Sensor, dgtl3,  leftEncoder,    sensorQuadEncoder)
-#pragma config(Motor,  port2,           rightMotor,    tmotorServoContinuousRotation, openLoop)
-#pragma config(Motor,  port3,           leftMotor,     tmotorServoContinuousRotation, openLoop, reversed)
+#pragma config(Sensor, dgtl1,  leftEncoder,    sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  rightEncoder,   sensorQuadEncoder)
+#pragma config(Motor,  port2,           rightMotor,    tmotorVex269, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorVex269, openLoop, reversed, encoder, encoderPort, dgtl1, 1000)
 
-int BASEVALUE = 540;
+void moveForward(int dist)
+{
+	SensorValue[rightEncoder] = 0;
+	SensorValue[leftEncoder] = 0;
 
-void moveForward(int time){
-  motor[rightMotor] = 127;
-  motor[leftMotor] = 127;
-  wait1Msec(time);
-  motor[rightMotor] = 0;
-  motor[leftMotor] = 0;
+	while(SensorValue[leftEncoder] < dist){
+
+		if(SensorValue[rightEncoder] == SensorValue[leftEncoder]){ // If rightEncoder has counted the same amount as leftEncoder:
+			// Move Forward
+			motor[rightMotor] = 80;
+			motor[leftMotor]  = 80;
+		}
+		else if(SensorValue[rightEncoder] > SensorValue[leftEncoder]){	// If rightEncoder has counted more encoder counts
+			// Turn slightly right
+			motor[rightMotor] = 60;
+			motor[leftMotor]  = 80;
+		}
+		else{	// Only runs if leftEncoder has counted more encoder counts
+			// Turn slightly left
+			motor[rightMotor] = 80;
+			motor[leftMotor]  = 60;
+		}
+	}
 }
-void moveBackwards(int time){
-  motor[rightMotor] = -127;
-  motor[leftMotor] = -127;
-  wait1Msec(time);
-  motor[rightMotor] = 0;
-  motor[leftMotor] = 0;
+
+void moveBackwards(int dist){
+	SensorValue[rightEncoder] = 0;
+	SensorValue[leftEncoder] = 0;
+
+	while(SensorValue[leftEncoder] > dist){
+
+		if(SensorValue[rightEncoder] == SensorValue[leftEncoder]){ // If rightEncoder has counted the same amount as leftEncoder:
+			motor[rightMotor] = -80;
+			motor[leftMotor]  = -80;
+		}
+		else if(SensorValue[rightEncoder] > SensorValue[leftEncoder]){	// If rightEncoder has counted more encoder counts
+			// Turn slightly right
+			motor[rightMotor] = -80;
+			motor[leftMotor]  = -60;
+		}
+		else{	// Only runs if leftEncoder has counted more encoder counts
+			// Turn slightly left
+			motor[rightMotor] = -60;
+			motor[leftMotor]  = -80;
+		}
+	}
 }
+
+void stopMotors(){
+	motor[rightMotor] = 0;
+  motor[leftMotor] = 0;
+  wait1Msec(2000);
+};
 
 task main()
 {
-  wait1Msec(2000);
+	int dist = 0;
 
-    //Clear Encoders
-  SensorValue[rightEncoder] = 0;
-  SensorValue[leftEncoder] = 0;
-
-  for (BASEVALUE; SensorValue[leftEncoder] < 2700; BASEVALUE += BASEVALUE){
-    moveForward(BASEVALUE);wait1Msec(1000);
-    moveBackwards(BASEVALUE);wait1Msec(1000);
-  }
-
+	for(int i = 0; i < 5; i++){
+			dist += 580;
+			moveForward(dist);
+			stopMotors();
+			moveBackwards(dist * -1);
+			stopMotors();
+	}
 }
