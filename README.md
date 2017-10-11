ddd02028f1c1e05cbbcb9cb6fcb9cb7681cbb25d
Aron Hrafnsson <aronhr98@gmail.com>
2017-10-11 12:01:16 +0000

    Added link of videos

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ROB2A3U
+
+### Linkur að videoum
+https://photos.app.goo.gl/AU5ArgbZfgCpnZ0j2
--- Verkefni4/sonar_saudakodi.c
+++ Verkefni4/sonar_saudakodi.c
@@ -1,12 +1,14 @@
 while(Btn7u && pushBtn == 0)
 {
+  if(stökt á ljósum){
   driveForward()
-  if(SonarSkynjarHættu)
-  {
-    left,right motor = 0
-    left,right motor -127
-    Bíða í 200ms
-    right motor = 127
-    left motor = -127
+    if(SonarSkynjarHættu)
+    {
+      left,right motor = 0
+      left,right motor -127
+      Bíða í 200ms
+      right motor = 127
+      left motor = -127
+    }
   }
 }
