ea6ada1d6e6de087db8ff7ececde0334218e5dcb
Aron Hrafnsson <aronhr98@gmail.com>
2017-12-14 23:26:44 +0000

    Edited file name and added some MD files

---
+++ Lokaverkefni/allProjects_saudarkodi.c
@@ -0,0 +1,22 @@
+task verkefni1(){
+	// koði úr verkefni 1
+}
+
+task verkefni2(){
+
+}
+
+task verkefni3(){
+
+}
+
+main(){
+	if(btn = 1){
+		startTask(verkefni1);
+	}
+	elseif(btn = 2){
+		startTask(verkefni2);
+	}elseif(btn = 3){
+		startTask(verkefni3);
+	}
+}
